01401adb76502be4691af8979024aea894c4cecf
Angel Mavare <angelmavare1@gmail.com>
2022-03-15 17:32:34 -0400

    partials

--- footer.php
+++ footer.php
@@ -15,36 +15,7 @@
 
 <!----END WRAPPER----->
 <footer>
-        <div class="w-100 footer-section" style="background: url('/assets/img/fondo-burbujas3.jpg');">
-            <div class="container position-relative card-newsletter-desk p-0" style="height:100px">
-                <div class="footer-overlay">
-
-                </div>
-                <div class="row w-100 m-0">
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
-
-                    </div>
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6" style="z-index:10">
-                        <div class="newsletter-section">
-                            <p><strong>¿Quieres se parte de la tripulación?</strong><br>
-                                Suscribete para mantenerte informado</p>
-                                <form>
-                                    <div class="mb-3 pt-2">
-  
-                                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Correo electrónico" style="max-width: 300px;margin: auto;">
-                                      
-                                    </div>
-                                    <button type="submit" class="btn btn-gradient mt-2">Me apunto</button>
-                                  </form>
-                        </div>
-                    </div>
-                </div>
-                <img class="card-tag-footer" src="/assets/img/card-footer.svg" alt="pixonauta subscribe">
-                
-            </div>
-            
-            
-        </div>
+		<?php get_template_part( 'template-parts/newsletter-partial' ); ?>
         <div class="row w-100 copyright-section" >
             <!-- Grid column -->
             <div class="col-md-6">
---
+++ template-parts/home-postlist-partial.php
@@ -0,0 +1,101 @@
+<section class="postlist">
+            <div class="container">
+                <!----TITLE SECTION BAR----->
+                <div>
+                    <div class="title-section">
+                        <img class="title-section-icon" src="/assets/img/card-gradient2.svg" alt="gradient">
+                        <div class="line-gradient-title"></div>
+                        <div class="content-title-section">
+                            <h2 class="text-white">Lo más nuevo</h2>
+                        </div>
+                    </div>
+                </div>
+                <!----END TITLE SECTION BAR----->
+
+                <div class="row">
+                    <div class="col-md-9">
+                        <!--  card -->
+                        <article class="card mb-4 horizontal-post-feed">
+                            <div class="row g-0">
+                                <div class="col-md-4 post-feed">
+                                    <a href="single.html" class="h-100  post-bottom-left post-top-left">
+                                        <picture>
+                                            <img class="post-img" src="/assets/img/img-test-1.jpg" alt="">
+                                        </picture>
+                                        
+                                    </a>
+
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <div class="mb-2">
+                                            <a class="text-orange text-decoration-none" href="#">Fecha, año</a><span class="text-orange">/</span><a class="text-orange fw-bold text-decoration-none" href="#">Categoria</a>
+                                        </div>
+                                        
+                                        <a href="single.html" class="link-text">
+                                            <h5 class="card-title" style="text-decoration: none;">Card title</h5>
+                                        
+                                            <p class="card-text">This is a wider card with supporting text below as a
+                                                natural lead-in to additional content. This content is a little bit longer.
+                                            </p>
+                                        </a>
+                                        <div class="ux-meta-post mt-2">
+                                            <img class="ux-profile-sm d-none d-sm-inline-block" src="/assets/img/profile.jpg" alt=""><small class="ux-post-editor"> Por <a class="link-profile" href="">Pixonauta</a></small>
+                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                        <!--  end card -->
+                        <!--  card -->
+                        <article class="card mb-4 horizontal-post-feed">
+                            <div class="row g-0">
+                                <div class="col-md-4 post-feed ">
+                                    <a href="single.html" class="h-100 post-bottom-left post-top-left ">
+                                        <picture>
+                                            <img class="post-img" src="/assets/img/img-test-1.jpg" alt="">
+                                        </picture>
+                                        
+                                    </a>
+
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <div class="mb-2">
+                                            <a class="text-orange text-decoration-none" href="#">Fecha, año</a><span class="text-orange">/</span><a class="text-orange fw-bold text-decoration-none" href="#">Categoria</a>
+                                        </div>
+                                        
+                                        <a href="single.html" class="link-text">
+                                            <h5 class="card-title" style="text-decoration: none;">Card title</h5>
+                                        
+                                            <p class="card-text">This is a wider card with supporting text below as a
+                                                natural lead-in to additional content. This content is a little bit longer.
+                                            </p>
+                                        </a>
+                                        <div class="ux-meta-post mt-2">
+                                            <img class="ux-profile-sm d-none d-sm-inline-block" src="/assets/img/profile.jpg" alt=""><small class="ux-post-editor"> Por <a class="link-profile" href="">Pixonauta</a></small>
+                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                        <!--  end card -->
+                        <div class="col-md-12 text-center">
+                            <a href="archive.html" class="btn btn-lg btn-gradient">Ver todos</a>
+                        </div>
+                    </div>
+                    <!--  SIDEBAR -->
+                    <div class="col-md-3">
+                        <aside>
+
+                        </aside>
+                    </div>
+                    <!-- END  SIDEBAR -->
+                </div>
+                
+            </div>
+
+
+
+
+        </section>
