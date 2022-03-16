46db994eff3fd9f82e37c4dc7c515c10d84d3d97
Angel Mavare <angelmavare1@gmail.com>
2022-03-15 20:53:41 -0400

    .

--- footer.php
+++ footer.php
@@ -18,13 +18,35 @@
 		<?php get_template_part( 'template-parts/newsletter-partial' ); ?>
         <div class="row w-100 copyright-section" >
             <!-- Grid column -->
-            <div class="col-md-6">
+            <div class="col-md-4">
 
                 <nav class="navbar navbar-expand-lg navbar-light bg-light text-primary">
                     
                     <div class="w-100">
-                      <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
+					<?php
+				wp_nav_menu( array(
+					'menu'            => 'politicas',
+					'theme_location'  => 'primary',
+					'depth'           => 2,                     
+					'menu_class'      => 'navbar-nav mr-auto menu-footer',
+					'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+					'walker'          => new WP_Bootstrap_Navwalker()
+				) );
+				?>
+                      
+                    </div>
+                  </nav>
+
+
+               
+            </div>
+            <!-- Grid column -->
+            <div class="col-md-4">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light text-primary">
+                    
+                    <div class="w-100 text-center">
+					<ul class="navbar-nav d-inline-block">
+                        <!-- <li class="nav-item active">
                           <a class="nav-link text-primary" href="#">Inicio <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
@@ -32,7 +54,7 @@
                         </li>
                         <li class="nav-item">
                           <a class="nav-link text-primary" href="#">Newsletter</a>
-                        </li>
+                        </li> -->
                         <li class="nav-item">
                            <!-- Facebook -->
                             <a class="rs-ic fb-ic nav-link text-primary" rel="noreferrer" href="https://www.facebook.com/pixonauta/" target="_blank">
@@ -49,15 +71,10 @@
                         
                       </ul>
                       
-                    </div>
-                  </nav>
-
-
-               
-            </div>
-            <!-- Grid column -->
-            
-            <div class="col-md-6">
+					</div>
+			</nav>
+			</div>
+            <div class="col-md-4">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light right-footer-nav">
                     
                     <div >
--- header.php
+++ header.php
@@ -71,31 +71,21 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
+
+					<?php
+						wp_nav_menu( array(
+							'theme_location'    => 'primary',
+							'depth'             => 2,
+							'container'         => 'div',
+							'container_class'   => 'collapse navbar-collapse',
+							'container_id'      => 'bs-example-navbar-collapse-1',
+							'menu_class'        => 'nav navbar-nav',
+							'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+							'walker'            => new WP_Bootstrap_Navwalker(),
+						) );
+						?>
+
+                    
                         <form class="form-inline d-flex" method="get" action="<?php bloginfo('url'); ?>"> <!-- echo home_url('/'); -->
                             <div class="input-group">
                                 <input type="text" class="form-control nebula-search" name="s" placeholder="Buscar" value="<?php the_search_query(); ?>" aria-label="Buscar">
